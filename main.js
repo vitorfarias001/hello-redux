b890fa5d4bf5bb935e2b01514da3ec35805b80fb
Vitor de Farias Metzner <contato.vitorfariass@gmail.com>
2021-03-02 22:35:26 -0300

    end exercise

---
+++ main.js
@@ -0,0 +1,29 @@
+'use strict'
+//return new state
+const counter = (state = 0, action) =>{
+    switch (action.type){
+        case 'INCREMENT': return state +1
+        case 'DECREMENT': return state -1 
+    }
+    return state
+}
+
+//object , save app   
+const {createStore} = Redux
+const store = createStore(counter)
+
+store.subscribe(() => {
+    $counter.textContent = store.getState()
+})
+const $counter = document.querySelector('[data-js="counter"]')
+const $decrement = document.querySelector('[data-js="decrement"')
+const $increment = document.querySelector('[data-js="increment"')
+$decrement.addEventListener('click', decrement, false)
+$increment.addEventListener('click', increment, false)
+function decrement(){
+    store.dispatch({type:'DECREMENT'})
+}
+
+function increment(){
+    store.dispatch({type:'INCREMENT'})
+}
